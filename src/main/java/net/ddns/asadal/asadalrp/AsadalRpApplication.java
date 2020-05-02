4078550b0fab7556ac9f98ad9bde43773480bb5d
kolyanpie <k0lyan.pie@gmail.com>
2020-05-02 21:29:22 +0300

    init project

---
+++ src/main/java/net/ddns/asadal/asadalrp/AsadalRpApplication.java
@@ -0,0 +1,13 @@
+package net.ddns.asadal.asadalrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsadalRpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsadalRpApplication.class, args);
+    }
+
+}
