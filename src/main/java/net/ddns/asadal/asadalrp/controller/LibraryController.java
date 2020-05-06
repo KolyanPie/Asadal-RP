55a4ed0b3811a16980bc42fa44bc2ed959464a9b
kolyanpie <k0lyan.pie@gmail.com>
2020-05-06 22:24:50 +0300

    add LibraryController

---
+++ src/main/java/net/ddns/asadal/asadalrp/controller/LibraryController.java
@@ -0,0 +1,15 @@
+package net.ddns.asadal.asadalrp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/admin")
+public class LibraryController {
+    @GetMapping("/library")
+    public ModelAndView library() {
+        return new ModelAndView("/static/admin/library.html");
+    }
+}
