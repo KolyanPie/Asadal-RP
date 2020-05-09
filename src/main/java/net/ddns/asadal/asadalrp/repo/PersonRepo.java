9b84afb47d0e4efcd5eba60051d458a1f996b351
kolyanpie <k0lyan.pie@gmail.com>
2020-05-09 17:09:07 +0300

    update repos

--- src/main/java/net/ddns/asadal/asadalrp/repo/PersonRepo.java
+++ src/main/java/net/ddns/asadal/asadalrp/repo/PersonRepo.java
@@ -3,6 +3,12 @@ package net.ddns.asadal.asadalrp.repo;
 import net.ddns.asadal.asadalrp.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepo extends JpaRepository<Person, Long> {
     Person findByName(String name);
+
+    List<Person> findAllByNameIgnoreCaseContains(String name);
+
+    List<Person> findAllByNameIgnoreCaseContainsAndPlayable(String name, boolean playable);
 }
