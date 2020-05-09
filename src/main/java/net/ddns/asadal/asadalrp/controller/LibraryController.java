d7bc1b593c4696eea037ad2a542bb159a5467848
kolyanpie <k0lyan.pie@gmail.com>
2020-05-09 17:04:15 +0300

    empty library domains logic

--- src/main/java/net/ddns/asadal/asadalrp/controller/LibraryController.java
+++ src/main/java/net/ddns/asadal/asadalrp/controller/LibraryController.java
@@ -1,12 +1,6 @@
 package net.ddns.asadal.asadalrp.controller;
 
-import net.ddns.asadal.asadalrp.domain.dto.ActionDto;
-import net.ddns.asadal.asadalrp.domain.dto.MoodletDto;
-import net.ddns.asadal.asadalrp.service.LibraryService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,38 +8,8 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/admin")
 public class LibraryController {
-    private final LibraryService libraryService;
-
-    public LibraryController(LibraryService libraryService) {
-        this.libraryService = libraryService;
-    }
-
     @GetMapping("/library")
     public ModelAndView library() {
         return new ModelAndView("/static/admin/library.html");
-    }
-
-    @PostMapping("/save/action")
-    public ResponseEntity<String> saveAction(
-            ActionDto action
-    ) {
-        String answer = libraryService.saveDomain(action);
-        if (answer.equals(action.getName())) {
-            return ResponseEntity.ok(answer);
-        } else {
-            return new ResponseEntity<>(answer, HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @PostMapping("/save/moodlet")
-    public ResponseEntity<String> saveAction(
-            MoodletDto moodlet
-    ) {
-        String answer = libraryService.saveDomain(moodlet);
-        if (answer.equals(moodlet.getName())) {
-            return ResponseEntity.ok(answer);
-        } else {
-            return new ResponseEntity<>(answer, HttpStatus.BAD_REQUEST);
-        }
     }
 }
