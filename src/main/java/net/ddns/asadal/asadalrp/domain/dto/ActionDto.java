e37a45ebc9ea9ff5621e21c0ec7244b01503cc99
kolyanpie <k0lyan.pie@gmail.com>
2020-05-09 20:49:53 +0300

    add id

--- src/main/java/net/ddns/asadal/asadalrp/domain/dto/ActionDto.java
+++ src/main/java/net/ddns/asadal/asadalrp/domain/dto/ActionDto.java
@@ -1,8 +1,17 @@
 package net.ddns.asadal.asadalrp.domain.dto;
 
 public class ActionDto {
+    private Long id;
     private String name;
     private String adminHint;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
--- src/main/java/net/ddns/asadal/asadalrp/domain/dto/MoodletDto.java
+++ src/main/java/net/ddns/asadal/asadalrp/domain/dto/MoodletDto.java
@@ -1,10 +1,19 @@
 package net.ddns.asadal.asadalrp.domain.dto;
 
 public class MoodletDto {
+    private Long id;
     private String name;
     private String description;
     private int durability;
     private byte value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
