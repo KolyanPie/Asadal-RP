2dd8452ba24fa094fa9f70cf8ba46a9d778e0338
kolyanpie <k0lyan.pie@gmail.com>
2020-06-02 18:59:51 +0300

    little fix

--- src/main/resources/static/js/preview.js
+++ src/main/resources/static/js/preview.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.preview').children('input[type="file"]').change(function () {
         const reader = new FileReader();
         reader.onload = function (e) {
-            $('.preview').children('img').attr('src', e.target.result);
+            $('.preview').find('img').attr('src', e.target.result);
         }
         reader.readAsDataURL(this.files[0]);
     });
